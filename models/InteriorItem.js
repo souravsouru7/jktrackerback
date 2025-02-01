a91822c080b097bfbd9f29c64a319e4a64d34564
sourav <soutavr5@gmail.com>
2025-02-01 17:39:25 +0530

    updatedeitbill

--- models/InteriorItem.js
+++ models/InteriorItem.js
@@ -7,7 +7,7 @@ const interiorItemSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
+        required: false
     },
     unit: {
         type: String,
